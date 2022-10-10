7c30da529805d60f209ee66533c53731dea91671
Budnikov Illia <ilyabudkov@gmail.com>
2022-10-10 21:59:10 +0300

    add new files

---
+++ src/main/java/com/example/springmvcdemo/SpringMVCbootApplication.java
@@ -0,0 +1,11 @@
+package com.example.springmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMVCbootApplication.class, args);
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ilyab
+  Date: 10.10.2022
+  Time: 21:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h2>Spring MVC Demo - Home Page</h2>
+</body>
+</html>
